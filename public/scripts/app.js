879998c3d2e626ef30ad8537949317334796bd68
Nicola <va3nky@hotmail.com>
2018-09-12 21:23:00 +0000

    implemented mongoDB and cleaned up code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,16 +5,16 @@
  */
 
 //creates the html
-function createTweetElement(db){
-  const $tweet = $("<article>").addClass("tweet");
-  const header = `<header class="tweet-header">
+function createTweetElement(db) {
+    const $tweet = $("<article>").addClass("tweet");
+    const header = `<header class="tweet-header">
                     <img class="avatar" src="${escape(db.user.avatars.small)}">
                     <strong class="userName">${escape(db.user.name)}</strong>
                     <small class="user">${escape(db.user.handle)}</small>
-                  </header>`
-  const body = `<p class="tweet-body">${escape(db.content.text)}</p>`;
-  const timeCreated = new Date(db.created_at).toLocaleTimeString();
-  const footer = `<footer class="timeStamp">${escape(timeCreated)}
+                  </header>`;
+    const body = `<p class="tweet-body">${escape(db.content.text)}</p>`;
+    const timeCreated = new Date(db.created_at).toLocaleTimeString();
+    const footer = `<footer class="timeStamp">${escape(timeCreated)}
                     <p class="icons">
                       <i class="fas fa-flag"></i>
                       <i class="fas fa-retweet"></i>
@@ -22,80 +22,80 @@ function createTweetElement(db){
                     </p>
                   </footer>`;
 
-  $tweet.append(header);
-  $tweet.append(body);
-  $tweet.append(footer);
+    $tweet.append(header);
+    $tweet.append(body);
+    $tweet.append(footer);
 
-  return $tweet;
+    return $tweet;
 
 }
 
 function renderTweet(a) {
-  a.forEach(function (tweet){
-    $("#tweets-container").prepend(createTweetElement(tweet));
-  });
+    a.forEach(function(tweet) {
+        $("#tweets-container").prepend(createTweetElement(tweet));
+    });
 }
 
 //tweet submit and error handling
 function tweetSubmit() {
-  const $error = $("#errorMessage");
-    $("#tweet-form").submit(function (event) {
+    const $error = $("#errorMessage");
+    $("#tweet-form").submit(function(event) {
         $error.hide({
-          opacity: "toggle"
+            opacity: "toggle"
         });
-        $(".new-tweet").css( {"padding-bottom": "35px"});
-      event.preventDefault();
-      if ($(".text-box").val().length > 140) {
-        $error.text("Error: Tweet cannot exceed 140 characters");
-        $error.show({
-          opacity: "toggle"
-        });
-        $(".new-tweet").css( {"padding-bottom": "25px"});
-      } else if (!$(".text-box").val().length){
-        $error.text("Error: Please enter a tweet");
-        $error.show({
-          opacity: "toggle"
-        });
-        $(".new-tweet").css( {"padding-bottom": "25px"});
-      } else {
-        $.ajax("/tweets", { data: $(this).serialize(),method: "POST",
-            success: data => {
-               loadTweets();
-            }
-          });
-      }
-      });
-  };
+        $(".new-tweet").css({ "padding-bottom": "35px" });
+        event.preventDefault();
+        if ($(".text-box").val().length > 140) {
+            $error.text("Error: Tweet cannot exceed 140 characters");
+            $error.show({
+                opacity: "toggle"
+            });
+            $(".new-tweet").css({ "padding-bottom": "25px" });
+        } else if (!$(".text-box").val().length) {
+            $error.text("Error: Please enter a tweet");
+            $error.show({
+                opacity: "toggle"
+            });
+            $(".new-tweet").css({ "padding-bottom": "25px" });
+        } else {
+            $.ajax("/tweets", {
+                data: $(this).serialize(),
+                method: "POST",
+                success: data => {
+                    loadTweets();
+                }
+            });
+        }
+    });
+};
 
 function loadTweets() {
     $.ajax("/tweets", { method: "GET" })
-    .then(function (tweets) {
-      renderTweet(tweets);
-    });
+        .then(function(tweets) {
+            renderTweet(tweets);
+        });
 }
 
 //helper function to help protect against XSS
 function escape(str) {
-  var div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
 }
 
-function toggleForm (){
-  const $button = $("#compose");
-  $button.on("click", function(){
-    $(".new-tweet").animate({
-      height: "toggle",
-      opacity: "toggle"
+function toggleForm() {
+    const $button = $("#compose");
+    $button.on("click", function() {
+        $(".new-tweet").animate({
+            height: "toggle",
+            opacity: "toggle"
+        });
+        $(".text-box").focus();
     });
-    $(".text-box").focus();
-  });
 }
 
 $(document).ready(function() {
-  tweetSubmit();
-  loadTweets();
-  toggleForm();
+    tweetSubmit();
+    loadTweets();
+    toggleForm();
 });
-
-
